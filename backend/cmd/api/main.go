187fa459bb5925101362e666e7b7ce830e1070a4
Alexandr Nefedzin <demo2886@yandex.by>
2024-08-27 01:37:36 +0300

    ds

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -13,6 +13,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/Demo2886/momo-store/backend/cmd/api/app"
+	"github.com/Demo2886/momo-store/backend/cmd/api/router"
 	"github.com/Demo2886/momo-store/backend/cmd/api/dependencies"
 	"github.com/Demo2886/momo-store/backend/internal/logger"
 )
