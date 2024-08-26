d6b0d33e3cbefb5619c8851102434ed59472704a
Alexandr Nefedzin <demo2886@yandex.by>
2024-08-27 00:28:40 +0300

    add

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -12,10 +12,10 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/Demo2886/momo-store/cmd/api/app"
-	"github.com/Demo2886/momo-store/cmd/api/dependencies"
-	"github.com/Demo2886/momo-store/cmd/api/router" //Импортируйте новый пакет
-	"github.com/Demo2886/momo-store/internal/logger"
+	"github.com/Demo2886/momo-store/backend/cmd/api/app"
+	"github.com/Demo2886/momo-store/backend/cmd/api/dependencies"
+	"github.com/Demo2886/momo-store/backend/cmd/api/router" //Импортируйте новый пакет
+	"github.com/Demo2886/momo-store/backend/internal/logger"
 )
 
 func main() {
